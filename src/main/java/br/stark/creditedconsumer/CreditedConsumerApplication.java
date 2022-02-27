a95dafe18edb92f1e7f74de0cb1b10f6064843a6
lucas-salves <salves.lucas@outlook.com>
2022-02-27 19:56:58 -0300

    first commit

---
+++ src/main/java/br/stark/creditedconsumer/CreditedConsumerApplication.java
@@ -0,0 +1,13 @@
+package br.stark.creditedconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditedConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditedConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/stark/creditedconsumer/CreditedConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.stark.creditedconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditedConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
